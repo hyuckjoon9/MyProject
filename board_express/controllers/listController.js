ff489a7d93793d093038357a9ca93d5f5b1bea1c
hyuckjoon9 <revo3325@gmail.com>
2025-05-12 02:27:21 +0900

    style: /list 디자인 수정 및 EJS 링크 정리

--- board_express/controllers/listController.js
+++ board_express/controllers/listController.js
@@ -1,5 +1,17 @@
 var express = require('express');
 
 exports.getList = (req, res) => {
-  res.render('list');
+  let testPosts = [
+    {
+      id: 1,
+      title: '첫번째 글',
+      content: '첫번째 내용',
+    },
+    {
+      id: 2,
+      title: '두번째 글',
+      content: '두번째 내용',
+    },
+  ];
+  res.render('list', { posts: testPosts });
 };
